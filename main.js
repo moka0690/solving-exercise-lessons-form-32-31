6f3abe1060fb59d92e23bfb2ad97e1746058791a
moka0690 <madradz39@gmail.com>
2024-03-06 21:54:55 +0100

    solveSolving exercise 3

--- main.js
+++ main.js
@@ -20,3 +20,12 @@ console.log( num2 >= num1 ); // true
 console.log( num1 < num2 ); // true
 
 
+let a = 20;
+let b = 30;
+let c = 10;
+
+console.log(a < b && a > c || a < b); // true
+
+console.log(a < b || a > c); // true
+
+console.log(!(a > b) && !(a > b) && !(a < c) && !(a == c)); // true
